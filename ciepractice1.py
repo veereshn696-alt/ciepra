ff3e7224b9b88a6c6a5bee62870a3b35e0981a06
veereshn696-alt <veereshn696@gmail.com>
2025-11-27 14:28:40 +0530

    Update ciepractice1.py

--- ciepractice1.py
+++ ciepractice1.py
@@ -1,10 +1,10 @@
 import sys
 if len(sys.argv)==5 :
-script_name=sys.argv[0]
-sub1=sys.argv[1]
-sub2=sys.argv[2]
-sub3=sys.argv[3]
-sub4=sys.argv[4]
+ script_name=sys.argv[0]
+ sub1=sys.argv[1]
+ sub2=sys.argv[2]
+ sub3=sys.argv[3]
+ sub4=sys.argv[4]
 else :
  script_name=sys.argv[0]
  sub1=20
@@ -15,13 +15,13 @@ print("sub 1 =",sub1)
 print("sub 2 =",sub2)
 print("sub 3 =",sub3)
 print("sub 4 =",sub4)
-avg=(sub1+sub2+sub3+sub4)/4
+avg=(sub1 + sub2 + sub3 + sub4)/4
 print("average=",avg)
 if(avg>35) :
   print("a")
 elif(avg>30) :
   print("b")
 elif(avg>20) :
-  print("c") :
+  print("c")
 else :
   print("d") 
